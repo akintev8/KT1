3b4735d39504d0408a81d23d88b30be0840e4c37
akintev8 <akintev8@gmail.com>
2024-02-26 12:36:08 +0500

    пОФИКСИЛ

--- src/Zad3.java
+++ src/Zad3.java
@@ -1,25 +1,32 @@
 public class Zad3 {
     public static void main(String[] args) {
-        String[] strs = {"dog","racecar","car"};
+        String[] strs = {"ass","asds","as","asdsds"};
         String word = strs[0];
         String res="";
+        boolean isChange=true;
         for (int i = 1; i < strs.length; i++) {
             if (strs[i].length() > word.length()) {
                 for (int j = 0; j < word.length(); j++) {
-                    if (word.charAt(j) != strs[i].charAt(j)) {
-                        res = word.substring(j, word.length() - 1);
+                    if (word.charAt(j) != strs[i].charAt(j)&& j==0) {
+                        word = "";
                         break;
+                    } else if (word.charAt(j) != strs[i].charAt(j)&& j!=0) {
+                        word=word.substring(0, word.length()-1);
                     }
                 }
             } else {
                 for (int j = 0; j < strs[i].length(); j++) {
-                    if (word.charAt(j) != strs[i].charAt(j)) {
-                        res = word.substring(0, j);
+                    if (word.charAt(j) != strs[i].charAt(j)&&j==0) {
+                        word = "";
+                        break;
+                    } else if (word.charAt(j) != strs[i].charAt(j)&&j!=0) {
+                        word = word.substring(0, j);
                         break;
                     }
                 }
             }
         }
-        System.out.println(res);
+        System.out.println(word);
+
     }
 }
